678e2825f033dd089cf74d3027b731e1b1a8b90c
Frederick Samanga <fredericktsamanga@gmail.com>
2025-01-07 11:03:14 +0200

    Done initial configuration and added data models

---
+++ src/main/java/com/jexpression/spotlightzim/SpotlightZimApplication.java
@@ -0,0 +1,13 @@
+package com.jexpression.spotlightzim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotlightZimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotlightZimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jexpression/spotlightzim/SpotlightZimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jexpression.spotlightzim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotlightZimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
